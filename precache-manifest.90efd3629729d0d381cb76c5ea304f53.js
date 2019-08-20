97ea2f0f5913042d1c6debd81de555da91f09fb5
devibrahem <dev.ibrahembastawi@gmail.com>
2019-08-20 22:11:29 +0200

    Updates

---
+++ precache-manifest.90efd3629729d0d381cb76c5ea304f53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a703e4616d36fa48c35ce1413af4ecc9",
+    "url": "/github-cards/index.html"
+  },
+  {
+    "revision": "9023d8f13e792933ca86",
+    "url": "/github-cards/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb3cc9c3bfb8b9cd8f01",
+    "url": "/github-cards/static/js/2.6cfadd2a.chunk.js"
+  },
+  {
+    "revision": "9023d8f13e792933ca86",
+    "url": "/github-cards/static/js/main.6e0658c2.chunk.js"
+  },
+  {
+    "revision": "cba495dac6c00947b956",
+    "url": "/github-cards/static/js/runtime~main.23cea6d9.js"
+  }
+]);
